20a38c8cc18f1b7bcecaba420f6308395695deca
Vedang <vedangssjgod@gmail.com>
2024-02-22 19:39:52 +0530

    This is A TicTacToe Project

---
+++ src/test/java/com/example/tictactoe/TictactoeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tictactoe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TictactoeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
