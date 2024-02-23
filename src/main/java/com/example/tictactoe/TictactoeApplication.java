a1234fc0ca4aac890ae556305d8d9a1a1bb70319
Vedang <vedangssjgod@gmail.com>
2024-02-23 19:17:27 +0530

    Script set for javascript backend

--- src/main/java/com/example/tictactoe/TictactoeApplication.java
+++ src/main/java/com/example/tictactoe/TictactoeApplication.java
@@ -2,12 +2,21 @@ package com.example.tictactoe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
 
 @SpringBootApplication
 public class TictactoeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TictactoeApplication.class, args);
+		TictactoeApplication t=new TictactoeApplication();
+		t.index();
 	}
-
+	public ModelAndView index () {
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("index.html");
+		return modelAndView;
+	}
 }
